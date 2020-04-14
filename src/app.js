5782218951a9789d629ad79954397c31c4eed383
Daedra <foglzerika@gmail.com>
2020-04-14 18:33:22 -0500

    add npm scripts, eslint, prettier, gitignore

--- src/app.js
+++ src/app.js
@@ -4,46 +4,33 @@
  2)Children : Whatever children how was passed .All the attributes that you want to give to the component
 
  */
-const Pet = ( {name, specie,breed}) =>{
-
-  return React.createElement("div", {},
-      [
-          React.createElement("h2", {} , name),
-          React.createElement("h2",{}, specie),
-          React.createElement("h2",{}, breed)
-
-      ]
-  )
+const Pet = ({ name, specie, breed }) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, name),
+    React.createElement("h2", {}, specie),
+    React.createElement("h2", {}, breed),
+  ]);
 };
 
-const App = ()=>{
-    return React.createElement(
-        "div",
-        {},
-        [
-            React.createElement("h1", {}, "Adopt me"),
-            React.createElement(Pet, {
-                name : "Sasha",
-                specie: "Dog",
-                breed:"GreatDane"
-            }),
-            React.createElement(Pet, {
-                name : "Munish",
-                specie: "Cat",
-                breed: "Mix"
-            }),
-            React.createElement(Pet, {
-                name : "Pepper",
-                specie: "Bird",
-                breed:"Cocktiel"
-            })
-
-        ]
-
-    )
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt me"),
+    React.createElement(Pet, {
+      name: "Sasha",
+      specie: "Dog",
+      breed: "GreatDane",
+    }),
+    React.createElement(Pet, {
+      name: "Munish",
+      specie: "Cat",
+      breed: "Mix",
+    }),
+    React.createElement(Pet, {
+      name: "Pepper",
+      specie: "Bird",
+      breed: "Cocktiel",
+    }),
+  ]);
 };
 
-ReactDOM.render(
-    React.createElement(App),
-    document.getElementById("root")
-);
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
