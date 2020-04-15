5a9dae6c3b88a99e22d9358780a76dba5cdec8b7
Daedra <foglzerika@gmail.com>
2020-04-15 14:12:00 -0500

    useState, custome hooks, update ESLint, bring in petfinder client

--- src/App.js
+++ src/App.js
@@ -1,33 +1,15 @@
-//All your code goes here
-/* The three parameters of .createElement methods parent
- 1)What kind of element it is
- 2)Children : Whatever children how was passed .All the attributes that you want to give to the component
-
- */
 import React from 'react';
 import { render } from 'react-dom';
-import Pet from "./Pet";
+
+import SearchParams from "./components/SearchParams";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt me"),
 
-    React.createElement(Pet, {
-      name: "Sasha",
-      specie: "Dog",
-      breed: "GreatDane",
-    }),
-    React.createElement(Pet, {
-      name: "Munish",
-      specie: "Cat",
-      breed: "Mix",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      specie: "Bird",
-      breed: "Cocktiel",
-    }),
-  ]);
+  return(
+      <div>
+          <h1 id="something-importand">Adopt Me!</h1>
+          <SearchParams/>
+      </div>
+  )
 };
-
-render(React.createElement(App), document.getElementById("root"));
+render(<App/>, document.getElementById("root"));
--- src/Pet.js
+++ src/Pet.js
@@ -1,11 +1,6 @@
 import React from "react";
 
 const Pet = ({ name, specie, breed }) => {
-   /* return React.createElement("div", {}, [
-        React.createElement("h2", {}, name),
-        React.createElement("h2", {}, specie),
-        React.createElement("h2", {}, breed),
-    ]);*/
    return(
        <div>
           <h2>{name}</h2>
